47ba241beb1c5495b3cfe1c727b7cb80629439f3
VijayaBhaskar <bhaskar289@gmail.com>
2021-12-04 12:22:57 +0530

    modified app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// Importing express module
+const express = require('express');
+const app = express();
 
+// Getting Request
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+	// Sending the response
+	res.send('Hello World!')
+	
+	// Ending the response
+	res.end()
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// Establishing the port
+const PORT = process.env.PORT ||5000;
+
+// Executing the server on given port number
+app.listen(PORT, console.log(
+`Server started on port ${PORT}`));
