18688c606738e9f667452eb4f34307917516e6e0
VijayaBhaskar <bhaskar289@gmail.com>
2022-01-06 11:54:54 +0530

    port

--- config.js
+++ config.js
@@ -2,10 +2,11 @@ const env = process.env;
 
 const config = {
   db: { /* don’t expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'db4free.net:3306',
+    host: env.DB_HOST || '85.10.205.173',
     user: env.DB_USER || 'familytree',
     password: env.DB_PASSWORD || 'Password#01',
     database: env.DB_NAME || 'familytree',
+    port: env.PORT || 3306,
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
