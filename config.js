ee7fb7fed93ba0765ab63d3ac33fa1ffe94e49a6
VijayaBhaskar <bhaskar289@gmail.com>
2022-01-06 11:48:01 +0530

    config port changed

--- config.js
+++ config.js
@@ -2,11 +2,10 @@ const env = process.env;
 
 const config = {
   db: { /* don’t expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'db4free.net',
+    host: env.DB_HOST || 'db4free.net:3306',
     user: env.DB_USER || 'familytree',
     password: env.DB_PASSWORD || 'Password#01',
     database: env.DB_NAME || 'familytree',
-    port: env.PORT || 3306,
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
