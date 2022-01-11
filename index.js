4bdef61353873ad4d1a37b6785e0ca8863bd7dac
VijayaBhaskar <bhaskar289@gmail.com>
2022-01-11 21:49:37 +0530

    remove / for comparision

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const passwordRouter = require('./routes/password');
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://myfamilytreeindia.netlify.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://myfamilytreeindia.netlify.app');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
