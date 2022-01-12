4c5930132209a512d59116371fdce8863d674da1
VijayaBhaskar <bhaskar289@gmail.com>
2022-01-12 15:59:34 +0530

    added *

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://myfamilytreeindia.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
